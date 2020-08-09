07bbdfbf3064460f6840dcfc499d6759136a70bf
Tiffany Santos <tiffanysantos@Tiffanys-MacBook-Air.local>
2020-08-09 14:25:40 +0200

    add shoelaces route

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
   return "Hello, world!"
 
+@app.route("/shoelaces")
+def shoelaces():
+  return "This works now!"
+
   if __name__ == '__main__':
     app.run(debug=True)
 
